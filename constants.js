135b96bc5716163bdc2a8063728ce85577b6c511
Tomer Raz <tomer@vegvizer.com>
2023-01-17 22:57:05 +0200

    first version of the project

---
+++ constants.js
@@ -0,0 +1,29 @@
+// day of departure
+export const DEPARTURE_DAY = 'thursday';
+
+// day of return
+export const RETURN_DAY = 'monday';
+
+// only direct flights
+export const DIRECT_FLIGHT = true;
+
+// number of months from now (to start the search). if set to 0, the search will start from today
+export const MONTHS_DELAY = 0;
+
+// city of departure
+export const DEPARTURE_CITY = 'Tel Aviv';
+
+// city-country, example: madrid-spain, london-united-kingdom
+export const DEPARTURE_CITY_URL_PARAM = 'tel-aviv-israel';
+
+// time (in seconds) between requests (so you will not get blocked by the server)
+export const DEFAULT_TIMEOUT = 5;
+
+// the currency you want to pay in
+export const CURRENCY_SYMBOL = '$';
+
+// maximum price you want to pay (for both flights combined)
+export const PRICE_THRESHOLD = 150;
+
+// number of weekends from now forward to search
+export const NUMBER_OF_WEEKENDS_TO_SEARCH = 5;
